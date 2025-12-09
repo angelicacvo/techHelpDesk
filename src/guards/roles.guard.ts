b0b38d23231d1db81949daafadaff3329a408f4b
Angelica Cuervo <108093638+angelicacvo@users.noreply.github.com>
2025-12-09 07:38:11 -0500

    feat: add role's guard

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -1,11 +1,36 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { ROLES_KEY } from '../decorator/roles.decorator';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
-export class GuardRoles implements CanActivate {
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) { }
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    if (!user || !user.roles) {
+      throw new Error('User not found or roles not defined');
+    }
+ 
+    const hasRole = () => user.roles.some((role: string) => requiredRoles.includes(role));
+    
+    if (!hasRole()) {
+      throw new Error('User does not have the required roles');
+    }
+    // if user has required roles, allow access
     return true;
   }
 }
