428dc3064cf0b8922f64e2519a7d1d8404bb847b
Angelica Cuervo <108093638+angelicacvo@users.noreply.github.com>
2025-12-09 08:43:03 -0500

    feat: database module implemented in main.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ClientsModule } from './clients/clients.module';
 import { TechniciansModule } from './technicians/technicians.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,24 +14,7 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USER'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true,
-        logging: false,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }),
-    }),
+    DatabaseModule,
     AuthModule,
     UsersModule,
     TicketsModule,
@@ -43,4 +26,5 @@ import { AuthModule } from './auth/auth.module';
   providers: [],
 })
 export class AppModule {}
+
 
