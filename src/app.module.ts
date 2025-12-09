a5211bd8b8cd96f5f05eb44d2b28204fd9e56950
Angelica Cuervo <108093638+angelicacvo@users.noreply.github.com>
2025-12-09 07:38:39 -0500

    feat: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ClientsModule } from './clients/clients.module';
 import { TechniciansModule } from './technicians/technicians.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, TicketsModule, CategoriesModule, ClientsModule, TechniciansModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    UsersModule, 
+    TicketsModule, 
+    CategoriesModule, 
+    ClientsModule, 
+    TechniciansModule, 
+    AuthModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
