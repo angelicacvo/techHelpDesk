e1e49849fa967c1a279316ee57583a1690b436e4
Angelica Cuervo <108093638+angelicacvo@users.noreply.github.com>
2025-12-09 09:58:35 -0500

    feat: technicians CRUD with endpoints protected and validations

--- src/technicians/technicians.module.ts
+++ src/technicians/technicians.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TechniciansService } from './technicians.service';
 import { TechniciansController } from './technicians.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Technician } from './entities/technician.entity';
+import { User } from '../users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Technician, User])],
   controllers: [TechniciansController],
   providers: [TechniciansService],
 })
