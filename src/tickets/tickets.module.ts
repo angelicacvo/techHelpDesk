91af6e27d24ad0271d259daeb9ebc1badee25f28
Angelica Cuervo <108093638+angelicacvo@users.noreply.github.com>
2025-12-10 08:25:15 -0500

    fix: Register custom validators as providers in TicketsModule

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -6,10 +6,18 @@ import { Ticket } from './entities/ticket.entity';
 import { Category } from '../categories/entities/category.entity';
 import { Client } from '../clients/entities/client.entity';
 import { Technician } from '../technicians/entities/technician.entity';
+import { CategoryExistsValidator } from '../common/validators/category-exists.validator';
+import { ClientExistsValidator } from '../common/validators/client-exists.validator';
+import { TechnicianWorkloadValidator } from '../common/validators/technician-workload.validator';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Ticket, Category, Client, Technician])],
   controllers: [TicketsController],
-  providers: [TicketsService],
+  providers: [
+    TicketsService,
+    CategoryExistsValidator,
+    ClientExistsValidator,
+    TechnicianWorkloadValidator,
+  ],
 })
 export class TicketsModule {}
